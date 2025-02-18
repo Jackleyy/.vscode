6859b0cfc0764561cc712becb0d2888cd64b87cc
Jackleyy <justin.ackind@gmail.com>
2025-02-18 13:17:16 -0800

    almost done, hopefully

--- Comsc-210/Jackley-Prog2.cpp
+++ Comsc-210/Jackley-Prog2.cpp
@@ -31,38 +31,73 @@ int main(){
     ifstream test("test.txt");
     string myText;
 
-    while(getline(test, myText)){
-      int num;
-      string tell;
-      stack<string> sab;
-      
 
+      int num; // tells how many lines to read
+      int index = 1; //will tell when to clear the stack for the next test; set to one because the continue will skip index++
+      string tell; //instruction; either i or r
+      stack<string> sab; //stack for the strings  
+      string temp; //for placing the popped string into for checking purposes
+      bool stack = true; //if it is a stack or not
+
+    while(getline(test, myText)){
       //if it is a number it will be put in num, if its not a number it must be 
       //a string to decipher so the first letter will be the instruction
-      if(is_number(myText)){
+
+      if(is_number(myText)){ //if nuymber, skip the rest of the code
         num = stoi(myText);
         cout << num;
+        continue;
       }
-      else{
+      else{//if not number, read the instruction
         tell = myText.substr(0,1);
-        cout << tell;
       }
 
-      
+      //we have instruction, use logic to compare the pops
       if(tell == "i"){
         sab.push(myText.substr(2));
       }
       else if (tell == "r"){
-        
+        temp = myText.substr(2);
+
+        if(!sab.empty()){// i did this ugly nested if because it was throwing 
+                         // a seg fault anytime i did the following line
+          if (temp != string(sab.top())){
+            stack = false;
+          }
+        }
+      }
+      else{
+        cout << "invalid character for reading, must be 'i' or 'r' " << endl;
+        stack = false;
+        break;
       }
         
+      cout << index << 90 << endl;
+      //end check; checks if it has reach end of desired readings and resets eveerything
+      if (index == num){
+        //check the result of the stack
+        if(stack){
+          cout << "stack" << endl;
+        }
+        else{
+          cout << "not stack" << endl;
+        }
+        stack = true;
+        index = 1;
 
-      
+        //clearing the stack
+        while(!sab.empty()){
+          sab.pop();
+        }
+        continue;
+      }
+
+      index++;
     }
 
 
 
-    test.close();
+    test.close(); //closing the file
 
     return 0;
 
